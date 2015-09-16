e88e0c873a6d4491f3c819a6e8db79a91f028235
John Henry <john@iamjohnhenry.com>
2015-09-15 20:46:43 -0700

    Add rudamentary tests

--- test/es6testrunner.js
+++ test/es6testrunner.js
@@ -1,2 +1,9 @@
-require('babel/register')({stage:0});
+require('babel/register')({
+  // This will override `node_modules` ignoring - you can alternatively pass
+  // an array of strings to be explicitly matched or a regex / glob
+  ignore: false
+  //Set the experimental proposal stage.
+  //More: (http://babeljs.io/docs/usage/experimental/)
+  ,stage:0
+});
 require('./test.js');
--- test/test.js
+++ test/test.js
@@ -1,56 +1,91 @@
 //Imports-----------------------------------------------------------------------
 import Understate from '..';
+import assert from 'assert';
 import {getStorage, setLatest, LATESTID} from './storage';
+//Utilities-------------------------------------------------------------------
+ var log = (state, id = undefined) => {
+   if(id !== undefined) setLatest(id, state);
+   console.capture(`ID: ${id}
+STATE: ${state}`);
+ };
+
+//Test0-------------------------------------------------------------------------
+{
+  describe('Constructor', function() {
+    describe('(Empty)', function () {
+      it('It should not have an initial value', function (done) {
+        const understate = new Understate();
+        understate.get().then(state=>{
+          assert.equal(state, undefined);
+          done();
+        })
+      });
+    });
+    describe('Initial Value', function () {
+      it('It should have an initial value. ', function (done) {
+        const understate = new Understate({initial:null});
+        understate.get().then(state=>{
+          assert.equal(state, null);
+          done();
+        })
+      });
+    });
+  });
+}
 //Test1-------------------------------------------------------------------------
 {
-  //State-----------------------------------------------------------------------
-  const DEFAULTSTATE = 0;
-  //State Mutators--------------------------------------------------------------
-  const constant = a => _ => a;
-  const add = a => b => a + b;
-  const initialize = constant(getStorage(LATESTID) || DEFAULTSTATE);
-  //Utilities-------------------------------------------------------------------
-  const log = (state, id = undefined) => {
-    if(id !== undefined) setLatest(id, state);
-    console.log(`ID: ${id}
-  STATE: ${state}`);
-  };
-  //Run-------------------------------------------------------------------------
-  (new Understate({index:true}))//Original Instance
-  .s(initialize).subscribe(log)//New Instance
-  .s(add(1)).unsubscribe()//Original Instance
-  .s(add(2)).subscribe(log)//New Instance
-  .s(add(3)).unsubscribe()//Original Instance
-  .s(add(4)).subscribe(log)//New Instance
-  .s(add(5)).unsubscribe()//Original Instance
-  .s(add(6)).subscribe(log)//New Instance
-  .s(add(7)).unsubscribe()//Original Instance
-  .s(add(8)).subscribe(log)//New Instance
-  .s(add(9)).unsubscribe();//Original Instance
+  describe('Susbscription', function() {
+    describe('(Empty)', function () {
+      it('...', function (done) {
+        const arr = [];
+        const capture = state=>arr.push(state);
+        //State---------------------------------------------------------------
+        const DEFAULTSTATE = 0;
+        //State Mutators--------------------------------------------------------
+        const constant = a => _ => a;
+        const add = a => b => a + b;
+        const initialize = constant(getStorage(LATESTID) || DEFAULTSTATE);
+        (new Understate({index:true}))//Original Instance
+        .s(initialize).subscribe(capture)//New Instance
+        .s(add(1)).unsubscribe()//Original Instance
+        .s(add(2)).subscribe(capture)//New Instance
+        .s(add(3)).unsubscribe()//Original Instance
+        .s(add(4)).subscribe(capture)//New Instance
+        .s(add(5)).unsubscribe()//Original Instance
+        .s(add(6)).subscribe(capture)//New Instance
+        .s(add(7)).unsubscribe()//Original Instance
+        .s(add(8)).subscribe(capture)//New Instance
+        .s(add(9)).unsubscribe();//Original Instance
+        assert.deepEqual(arr, [1, 6, 15, 28, 45]);
+        done();
+      });
+    });
+  });
 }
 //Test2-------------------------------------------------------------------------
 {
-  var log = value => console.log(value);
-  //Builders
-  var constant    = a => _ => a;
-  var addMessageAsync  = message => messages => new Promise((resolve, reject)=>{
-    if(Math.random() < 0.25) return reject(new Error('Simulated Async Failure'));
-    return setTimeout(()=>resolve(messages.concat(message)), 1000);
+  describe('Async', function() {
+    describe('(Empty)', function () {
+      it('...', function (done) {
+        //Builders
+        var constant    = a => _ => a;
+        var addMessageAsync  = message => messages => new Promise((resolve, reject)=>{
+        //  if(Math.random() < 0.25) return reject(new Error('Simulated Async Failure'));
+          return setTimeout(()=>resolve(messages.concat(message)), 0);
+        });
+        //Mutators
+        var empty       = constant([]);
+        //App
+        var messages    = new Understate({asynchronous:true});
+        messages.set(empty,{asynchronous:false});
+        messages.set(addMessageAsync('Hello'))
+          .then(_=>messages.set(addMessageAsync('there'))
+          .then(_=>messages.set(addMessageAsync('John.'))
+          .then(()=>messages.get().then(result=>{
+            assert.deepEqual(result, ["Hello","there","John."]);
+            done();
+          }))));
+      });
+    });
   });
-  //Mutators
-  var empty       = constant([]);
-  //App
-  var messages    = new Understate({asynchronous:true});
-  messages.subscribe(log);
-  messages.set(empty,{asynchronous:false});
-  messages.set(addMessageAsync('Hello'))
-    .then(_=>messages.set(addMessageAsync('there'))
-    .then(_=>messages.set(addMessageAsync('John.'))))
-  .catch(log).catch(log);
-  //[]
-  //['Hello']
-  //['Hello', 'there']
-  //['Hello', 'there', 'John.']
-  //OR
-  //[Error: Simulated Async Failure]
 }
